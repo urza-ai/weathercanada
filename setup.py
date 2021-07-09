dcf6c37ff7af8bf05393edbf13413755e7083c0d
urza-ai <nikita@urza.ai>
2021-07-09 19:25:43 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='"A python package to download Canadian weather forecast and historical data from weather.gc.ca"',
+    author='Nikita@Urza.AI',
+    license='MIT',
+)
